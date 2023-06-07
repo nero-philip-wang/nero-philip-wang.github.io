4486f0e725a0c929d05b254c9df0c1ae04897114
nero.wang <nero.wang@re4.top>
2023-06-07 17:53:31 +0800

    config: 🔧增加head的script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,5 +6,7 @@ module.exports = {
     lastUpdated: "最后更新", // string | boolean
     // displayAllHeaders: true, // 默认值：false
   },
+  head: [['script', { async: true, src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0095302828244181', crossorigin: 'anonymous' }
+  ]],
   plugins: ['autobar']
 };
